745663e2d0dbfa2c992805abf241b37263dc5639
Octavio Harris <harris.octavio@gmail.com>
2018-04-28 01:08:04 +0500

    Added bash script to handle screen wake/sleep and syncing the repo
    Signed-off-by: Octavio Harris <harris.octavio@gmail.com>

---
+++ scripts/commands.sh
@@ -0,0 +1,14 @@
+#!/bin/bashh
+
+command="$1"
+
+if [ -z "$command" ] ; then 
+    exit 1
+elif [ "$command" ==  "screen-on" ] ; then
+    xset dpms force on
+elif [ "$command" ==  "screen-off" ] ; then
+    xset dpms force off
+elif [ "$command" ==  "update-repo" ] ; then
+    ./sync_repo.sh 
+else
+    exit 1
