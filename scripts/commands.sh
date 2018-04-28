08ee1bead741b6cf183bd10714e365928267a269
Octavio Harris <harris.octavio@gmail.com>
2018-04-28 18:47:34 +0500

    Actually wakes the screen properly
    Signed-off-by: Octavio Harris <harris.octavio@gmail.com>

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -5,7 +5,7 @@ command="$1"
 if [ -z "$command" ] ; then 
     exit 1
 elif [ "$command" ==  "screen-on" ] ; then
-    xset dpms force on
+    xset s activate
 elif [ "$command" ==  "screen-off" ] ; then
     xset dpms force off
 elif [ "$command" ==  "update-repo" ] ; then
