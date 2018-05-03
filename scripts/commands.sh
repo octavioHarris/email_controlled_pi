858194e7d28ba1b487c295328577739b89c495d3
Octavio Harris <harris.octavio@gmail.com>
2018-05-03 18:28:24 +0500

    Reverted command to force screen on.
    Signed-off-by: Octavio Harris <harris.octavio@gmail.com>

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -5,7 +5,7 @@ command="$1"
 if [ -z "$command" ] ; then 
     exit 1
 elif [ "$command" ==  "screen-on" ] ; then
-    xset s activate
+    xset dpms force on	
 elif [ "$command" ==  "screen-off" ] ; then
     xset dpms force off
 elif [ "$command" ==  "update-repo" ] ; then
